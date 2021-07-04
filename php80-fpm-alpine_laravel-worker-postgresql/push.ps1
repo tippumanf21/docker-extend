05190f7e9c56b57437e3a40b99c1d14b3e5337c6
kenji yamamoto <k.yamamoto.wk@gmail.com>
2021-07-04 17:35:53 +0900

    no cache

--- php80-fpm-alpine_laravel-worker-postgresql/push.ps1
+++ php80-fpm-alpine_laravel-worker-postgresql/push.ps1
@@ -1,4 +1,4 @@
-docker build -t kyamamoto0work/php80-fpm-alpine_laravel-worker-postgresql .
+docker build --no-cache -t kyamamoto0work/php80-fpm-alpine_laravel-worker-postgresql .
 docker login
 docker push kyamamoto0work/php80-fpm-alpine_laravel-worker-postgresql:latest
 pause
